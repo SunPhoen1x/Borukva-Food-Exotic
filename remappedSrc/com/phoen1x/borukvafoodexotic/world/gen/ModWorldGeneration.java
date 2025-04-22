d08f291f7302cc091c4c000eeece4d0d615aad3f
Ruslan <ruslanmur2007@gmail.com>
2025-04-22 15:02:48 +0300

    1.21.4

--- remappedSrc/com/phoen1x/borukvafoodexotic/world/gen/ModWorldGeneration.java
+++
@@ -1,7 +0,0 @@
-package com.phoen1x.borukvafoodexotic.world.gen;
-
-public class ModWorldGeneration {
-    public static void generateModWorldGen() {
-        ModTreeGeneration.generateTrees();
-    }
-}
